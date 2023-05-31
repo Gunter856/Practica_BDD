7c7a0d993004f8b0087fec137ee990b40b20139d
Iker <114989036+Gunter856@users.noreply.github.com>
2023-05-31 09:26:19 +0200

    Add files via upload

---
+++ fizzbuzz/features/steps/fizzbuzz.py
@@ -0,0 +1,12 @@
+class FizzBuzz:
+  @staticmethod
+  def calculates(num):
+    num = int(num)
+    result = ""
+    if num % 3 == 0:
+      result += "Fizz"
+    if num % 5 == 0:
+      result += "Buzz"
+    return result if result else f"{num}"
+
+
